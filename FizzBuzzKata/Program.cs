8f3e4ab00fa2bb43ecddde8f207cb7f7d0052abf
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-02-17 14:00:31 +0000

    Basic skeleton console app plus readme to explain the problem.

---
+++ FizzBuzzKata/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FizzBuzzKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Enter 'end' when you have finished");
+
+            string newInput = Console.ReadLine();
+
+            while (null != newInput && "END" != newInput.ToUpper())
+            {
+                newInput = Console.ReadLine();
+            }
+
+            Thread.Sleep(System.TimeSpan.FromSeconds(2));
+        }
+    }
+}
